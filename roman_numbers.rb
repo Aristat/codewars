d43d071bca92b2fdf8ae52ad8259d64d1b116771
Alexandr Strelnikov <aristat37@gmail.com>
2017-12-02 04:44:07 +0300

    roman number

---
+++ roman_numbers.rb
@@ -0,0 +1,26 @@
+ALPHABET = [
+  ["M", 1000],
+  ["CM", 900],
+  ["D", 500],
+  ["CD", 400],
+  ["C", 100],
+  ["XC", 90],
+  ["L", 50],
+  ["XL", 40],
+  ["X", 10],
+  ["IX", 9],
+  ["V", 5],
+  ["IV", 4],
+  ["I", 1]
+]
+
+def to_arabic(roman)
+  reply = 0
+  ALPHABET.each do |key, value|
+    while roman.index(key) == 0
+      reply += value
+      roman.slice!(key)
+    end
+  end
+  reply
+end
