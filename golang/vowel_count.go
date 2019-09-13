f4946cdfdeea900ba0291b04d0da376ea724fd3e
Alexander <aristat37@gmail.com>
2019-09-13 19:47:09 +0300

    Added vowel count kata

---
+++ golang/vowel_count.go
@@ -0,0 +1,25 @@
+/*
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, and u as vowels for this Kata.
+
+The input string will only consist of lower case letters and/or spaces.
+
+*/
+
+package kata
+
+func GetCount(str string) (count int) {
+  m := map[string]bool{"a": true, "e": true, "i": true, "o": true, "u": true}
+  
+  for _, item := range str {
+    _, ok := m[string(item)]
+    if ok {
+      count += 1
+    }
+    
+  }
+
+  return count
+}
