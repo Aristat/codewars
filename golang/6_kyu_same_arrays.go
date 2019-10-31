3963152d1e2949985657c7e6adff4f9d101de785
Alexander Strelnikov <aristat37@gmail.com>
2019-10-31 11:47:00 +0300

    Added kyu for golang files

---
+++ golang/6_kyu_same_arrays.go
@@ -0,0 +1,43 @@
+/*
+
+Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities. 
+"Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+Remarks
+a or b might be [] (all languages except R, Shell). a or b might be nil or null or None or nothing (except in Haskell, Elixir, C++, Rust, R, Shell, PureScript).
+
+If a or b are nil (or null or None), the problem doesn't make sense so return false.
+
+If a or b are empty then the result is self-evident.
+
+a or b are empty or not empty lists.
+
+*/
+
+package kata
+
+import (
+  "sort"
+  "reflect"
+)
+
+func Comp(array1 []int, array2 []int) bool {
+  if array1 == nil || array2 == nil || len(array1) != len(array2) {
+    return false
+  }
+  
+  newArray := []int{}
+
+  for _, number := range array1 {
+    newArray = append(newArray, number*number) 
+  }  
+  
+  sort.Ints(newArray)
+  sort.Ints(array2)
+  return reflect.DeepEqual(newArray, array2)
+}
