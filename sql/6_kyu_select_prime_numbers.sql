326309472ff016b6582b29927d0cb4bf3a8d7cc9
Alexander Strelnikov <aristat37@gmail.com>
2019-11-05 22:22:41 +0300

    Added sql kata

---
+++ sql/6_kyu_select_prime_numbers.sql
@@ -0,0 +1,7 @@
+SELECT table1.a AS prime
+FROM generate_series(2, 100) AS table1(a)
+WHERE NOT EXISTS (
+  SELECT table2.a
+  FROM generate_series(2, table1.a-1) AS table2(a)
+  WHERE table1.a % table2.a = 0
+);
