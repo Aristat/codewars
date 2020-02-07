2d8428fb301f4aaab2ac4457270bda97ca2abeca
Alexander Strelnikov <aristat37@gmail.com>
2020-02-07 21:10:38 +0300

    Added kyu ruby

---
+++ ruby/6_kyu_array_exchange.rb
@@ -0,0 +1,26 @@
+=begin
+
+Array Exchange and Reversing
+
+It's time for some array exchange! The objective is simple: exchange the elements of two arrays in-place in a way that their new content is also reversed.
+
+# before
+my_array = ['a', 'b', 'c']
+other_array = [1, 2, 3]
+
+my_array.exchange_with!(other_array)
+
+# after
+my_array == [3, 2, 1]
+other_array == ['c', 'b', 'a']
+
+=end
+
+class Array
+  def exchange_with!(other_array)
+    old_array = self.clone
+    self.replace(other_array.reverse)
+    other_array.clear
+    old_array.reverse.each { |i| other_array << i }
+  end
+end
