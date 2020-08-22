28022732ac9a1567602db09dc8b7f7de7c8d4f93
Alexander Strelnikov <aristat37@gmail.com>
2020-08-22 14:04:31 +0300

    Added new kyu

---
+++ golang/7_kyu_simple_remove_duplicates.go
@@ -0,0 +1,25 @@
+/*
+
+In this Kata, you will remove the left-most duplicates from a list of integers and return the result.
+
+// Remove the 3's at indices 0 and 3
+// followed by removing a 4 at index 1
+solve([3, 4, 4, 3, 6, 3]); // => [4, 6, 3]
+
+*/
+
+package kata 
+
+func Solve(arr []int) []int {
+  encountered := map[int]bool{}
+  result := []int{}
+
+  for i := len(arr) - 1; i >= 0; i-- {
+    if encountered[arr[i]] != true {
+      encountered[arr[i]] = true
+      result = append([]int{arr[i]}, result...)
+    }
+  }
+  
+  return result
+}
