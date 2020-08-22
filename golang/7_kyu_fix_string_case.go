1ebb99e0f66f1a669ab14c6642c71c16e1b557ab
Alexander Strelnikov <aristat37@gmail.com>
2020-08-22 13:10:57 +0300

    Added new kyu

---
+++ golang/7_kyu_fix_string_case.go
@@ -0,0 +1,41 @@
+/*
+
+In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+make as few changes as possible.
+if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+For example:
+
+solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+
+*/
+
+package kata
+
+import (
+	"strings"
+	"unicode"
+)
+
+func solve(str string) string {
+	countLower := 0
+	countUpper := 0
+
+	for _, char := range str {
+		if unicode.IsLower(char) {
+			countLower += 1
+		} else {
+			countUpper += 1
+		}
+	}
+
+	if countLower >= countUpper {
+		str = strings.ToLower(str)
+	} else {
+		str = strings.ToUpper(str)
+	}
+  
+  return str
+}
