c3f343024e503f8d304f6418a73aa19e43412367
Alexander Strelnikov <aristat37@gmail.com>
2020-08-25 10:45:50 +0300

    Added new kyu

---
+++ golang/8_kyu_string_repeat.go
@@ -0,0 +1,22 @@
+/*
+
+Write a function called repeat_str which repeats the given string src exactly count times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+ */
+
+package kata
+
+func RepeatStr(repetitions int, value string) string {
+	n := 0
+	result := ""
+
+	for n < repetitions {
+		result += value
+		n += 1
+	}
+
+	return result
+}
