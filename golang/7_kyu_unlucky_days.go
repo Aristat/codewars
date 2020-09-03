6276d10beac06ccad121ff428615ffcb04c8a474
Alexander Strelnikov <aristat37@gmail.com>
2020-09-04 00:07:15 +0300

    Added new 7 kyu kata

---
+++ golang/7_kyu_unlucky_days.go
@@ -0,0 +1,36 @@
+/*
+
+Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+
+Find the number of Friday 13th in the given year.
+
+Input: Year as an integer.
+
+Output: Number of Black Fridays in the year as an integer.
+
+Examples:
+
+unluckyDays(2015) == 3
+unluckyDays(1986) == 1
+
+ */
+
+package kata
+
+import (
+	"time"
+)
+
+func UnluckyDays(year int) int {
+	count := 0
+
+	for i := 1; i <= 12; i++ {
+		weekday := time.Date(year, time.Month(i), 13, 0, 0, 0, 0, time.UTC)
+
+		if weekday.Weekday().String() == "Friday" {
+			count += 1
+		}
+	}
+
+	return count
+}
