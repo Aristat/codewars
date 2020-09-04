378b4a6d0daa3f263f7d96515804bd1182a6599e
Alexander Strelnikov <aristat37@gmail.com>
2020-09-04 10:32:19 +0300

    Added new kyu golang

---
+++ golang/6_kyu_bit_counting.go
@@ -0,0 +1,29 @@
+/*
+
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number.
+You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+ */
+
+package kata
+
+import (
+	"strconv"
+)
+
+func CountBits(n uint) int {
+	var one rune = 49
+	count := 0
+
+	bs := strconv.FormatInt(int64(n), 2)
+
+	for _, char := range bs {
+		if char == one {
+			count += 1
+		}
+	}
+
+	return count
+}
