3974d43414cb6051bc98ed3df6f159b5da22b723
Alexander Strelnikov <aristat37@gmail.com>
2026-03-15 17:10:50 +0300

    roman to integer

---
+++ typescript/roman-to-integer.ts
@@ -0,0 +1,40 @@
+const mapping = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
+function romanToInt(s: string): number {
+    const characters = s.split('')
+    const length = characters.length
+    let total = 0
+    let cache = 0
+    for (let i = 0; i < characters.length; i++) {
+        if (i == length - 1) {
+            cache += mapping[characters[i]]
+            continue
+        }
+
+        if (mapping[characters[i]] < mapping[characters[i + 1]]) {
+            cache -= mapping[characters[i]]
+        } else {
+            cache += mapping[characters[i]]
+            total += cache
+            cache = 0
+        }
+    }
+
+    if (cache != 0) {
+        total += cache
+    }
+
+    return total
+};
+
+console.log(romanToInt('III'))
+console.log(romanToInt('LVIII'))
+console.log(romanToInt('MCMXCIV'))
