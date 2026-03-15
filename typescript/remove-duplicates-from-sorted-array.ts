15938dd9bacd72468cc67ca3491584b0048d234e
Alexander Strelnikov <aristat37@gmail.com>
2026-03-15 17:38:36 +0300

    remove duplicates from sorted array

---
+++ typescript/remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,21 @@
+function removeDuplicates(nums: number[]): number {
+    let previousNum = nums[0]
+    let uniqArray = []
+    let count = 1
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== previousNum) {
+            previousNum = nums[i]
+            uniqArray.push(nums[i])
+            count++
+        }
+    }
+
+    nums.splice(1, nums.length, ...new Set(uniqArray))
+    return count
+};
+
+let nums1 = [1, 1, 2]
+console.log(removeDuplicates(nums1), nums1)
+
+let nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+console.log(removeDuplicates(nums2), nums2)
